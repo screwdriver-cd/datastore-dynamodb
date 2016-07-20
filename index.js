7939ff959670e485db845d768dc857acfb5101ae
Darren Matsumoto <aeneascorrupt@gmail.com>
2016-07-20 15:14:56 -0700

    get data from dynamodb by primary key

--- index.js
+++ index.js
@@ -4,24 +4,63 @@ const schemas = require('screwdriver-data-schema');
 const vogels = require('vogels');
 
 const DEFAULT_REGION = 'us-west-2';
-// vogels.AWS.config.update({region: "REGION"}); // region must be set
+const TABLE_SCHEMAS = {
+    builds: schemas.build.base,
+    jobs: schemas.job.base,
+    pipelines: schemas.pipeline.base,
+    platforms: schemas.platform.base,
+    users: schemas.user.base
+};
 
 class Dynamodb extends Datastore {
+    /**
+     * Constructs a Dynamodb object
+     * @param  {Object} [config]         Configuration object
+     * @param  {String} [config.region]  AWS region to operate in
+     */
     constructor(config) {
-        const tableName = config.tableName;
-        const region = config.region || DEFAULT_REGION;
+        let region = DEFAULT_REGION;
+
+        if (config && config.region) {
+            region = config.region;
+        }
 
         super();
         vogels.AWS.config.update({ region });
 
-        this.client = vogels.define(tableName, {
-            hashKey: 'id',
-            schema: schemas[tableName].base
+        this.client = {};
+        Object.keys(TABLE_SCHEMAS).forEach((table) => {
+            this.client[table] = vogels.define(table, {
+                hashKey: 'id',
+                schema: TABLE_SCHEMAS[table],
+                tableName: table
+            });
         });
     }
 
+    /**
+     * Obtain an item from the DynamoDB table by primary key
+     * @param  {Object}   config       Configuration object
+     * @param  {String}   config.table Name of the table to interact with
+     * @param  {Object}   config.id    ID of the entry to fetch
+     * @param  {Function} callback     fn(err, data)
+     *                                 err - Error object
+     *                                 data - data from the table
+     */
     get(config, callback) {
-        callback(null);
+        const client = this.client[config.table];
+
+        if (!client) {
+            const err = new Error(`invalid table name "${config.table}"`);
+
+            return callback(err);
+        }
+
+        return client.get(config.id, (err, data) => {
+            const result = (data) ? data.toJSON() : null;
+
+            return callback(err, result);
+        });
     }
 
     save(config, callback) {
--- test/index.test.js
+++ test/index.test.js
@@ -6,8 +6,11 @@ const sinon = require('sinon');
 sinon.assert.expose(assert, { prefix: '' });
 
 describe('index test', () => {
+    let clientMock;
+    let datastore;
     let Datastore;
-    let schemaMock;
+    let responseMock;
+    let dataSchemaMock;
     let vogelsMock;
 
     before(() => {
@@ -18,12 +21,21 @@ describe('index test', () => {
     });
 
     beforeEach(() => {
-        schemaMock = {
-            tableName: {
-                base: sinon.stub()
-            }
+        responseMock = {
+            toJSON: sinon.stub()
         };
-        mockery.registerMock('screwdriver-data-schema', schemaMock);
+        clientMock = {
+            get: sinon.stub()
+        };
+
+        dataSchemaMock = {
+            build: { base: sinon.stub() },
+            job: { base: sinon.stub() },
+            pipeline: { base: sinon.stub() },
+            platform: { base: sinon.stub() },
+            user: { base: sinon.stub() }
+        };
+        mockery.registerMock('screwdriver-data-schema', dataSchemaMock);
 
         vogelsMock = {
             AWS: {
@@ -31,16 +43,15 @@ describe('index test', () => {
                     update: sinon.stub()
                 }
             },
-            define: sinon.stub(),
-            types: {
-                uuid: sinon.stub()
-            }
+            // warning: the same client mock is passed back for all tables in these tests
+            define: sinon.stub().returns(clientMock)
         };
         mockery.registerMock('vogels', vogelsMock);
 
         /* eslint-disable global-require */
         Datastore = require('../index');
         /* eslint-enable global-require */
+        datastore = new Datastore();
     });
 
     afterEach(() => {
@@ -52,28 +63,133 @@ describe('index test', () => {
         mockery.disable();
     });
 
-    it('constructs the client correctly', () => {
-        const datastore = new Datastore({ tableName: 'tableName' });
-
-        assert.isOk(datastore);
-        assert.calledWith(vogelsMock.define, 'tableName', {
-            hashKey: 'id',
-            schema: schemaMock.tableName.base
+    describe('constructor', () => {
+        it('constructs the client with the default region', () => {
+            assert.calledWith(vogelsMock.AWS.config.update, {
+                region: 'us-west-2'
+            });
         });
-        assert.calledWith(vogelsMock.AWS.config.update, {
-            region: 'us-west-2'
+
+        it('constructs the builds client', () => {
+            assert.calledWith(vogelsMock.define, 'builds', {
+                hashKey: 'id',
+                schema: dataSchemaMock.build.base,
+                tableName: 'builds'
+            });
+        });
+
+        it('constructs the jobs client', () => {
+            assert.calledWith(vogelsMock.define, 'jobs', {
+                hashKey: 'id',
+                schema: dataSchemaMock.job.base,
+                tableName: 'jobs'
+            });
+        });
+
+        it('constructs the pipelines client', () => {
+            assert.calledWith(vogelsMock.define, 'pipelines', {
+                hashKey: 'id',
+                schema: dataSchemaMock.pipeline.base,
+                tableName: 'pipelines'
+            });
+        });
+
+        it('constructs the platforms client', () => {
+            assert.calledWith(vogelsMock.define, 'platforms', {
+                hashKey: 'id',
+                schema: dataSchemaMock.platform.base,
+                tableName: 'platforms'
+            });
+        });
+
+        it('constructs the users client', () => {
+            assert.calledWith(vogelsMock.define, 'users', {
+                hashKey: 'id',
+                schema: dataSchemaMock.user.base,
+                tableName: 'users'
+            });
+        });
+
+        it('constructs the client with a defined region', () => {
+            datastore = new Datastore({
+                region: 'my-region'
+            });
+
+            assert.calledWith(vogelsMock.AWS.config.update, {
+                region: 'my-region'
+            });
         });
     });
 
-    it('constructs the client with a defined region', () => {
-        const datastore = new Datastore({
-            tableName: 'tableName',
-            region: 'my-region'
+    describe('get', () => {
+        let pipelinesClientMock;
+
+        beforeEach(() => {
+            pipelinesClientMock = {
+                get: sinon.stub()
+            };
+            vogelsMock.define = sinon.stub().withArgs('pipelines').returns(pipelinesClientMock);
+
+            datastore = new Datastore();
         });
 
-        assert.isOk(datastore);
-        assert.calledWith(vogelsMock.AWS.config.update, {
-            region: 'my-region'
+        it('gets data by id', (done) => {
+            const testParams = {
+                table: 'pipelines',
+                id: 'someId'
+            };
+            const testData = {
+                id: 'data',
+                key: 'value'
+            };
+
+            pipelinesClientMock.get.yieldsAsync(null, responseMock);
+            responseMock.toJSON.returns(testData);
+
+            datastore.get(testParams, (err, data) => {
+                assert.isNull(err);
+                assert.deepEqual(testData, data);
+                assert.calledWith(pipelinesClientMock.get, testParams.id);
+                done();
+            });
+        });
+
+        it('gracefully understands that no one is returned when it does not exist', (done) => {
+            pipelinesClientMock.get.yieldsAsync();
+
+            datastore.get({
+                table: 'pipelines',
+                id: 'someId'
+            }, (err, data) => {
+                assert.isNotOk(err);
+                assert.isNotOk(data);
+                done();
+            });
+        });
+
+        it('fails when given an unknown table name', (done) => {
+            datastore.get({
+                table: 'tableUnicorn',
+                id: 'doesNotMatter'
+            }, (err, data) => {
+                assert.match(err.message, /invalid table name/);
+                assert.isNotOk(data);
+                done();
+            });
+        });
+
+        it('fails when it encounters an error', (done) => {
+            const testError = new Error('errorCommunicatingToApi');
+
+            pipelinesClientMock.get.yieldsAsync(testError);
+            datastore.get({
+                table: 'pipelines',
+                id: 'someId'
+            }, (err, data) => {
+                assert.strictEqual(testError.message, err.message);
+                assert.isNotOk(data);
+                done();
+            });
         });
     });
 });
